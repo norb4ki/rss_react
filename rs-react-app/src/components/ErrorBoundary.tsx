efd4e7328c19518141bdd25d710d77ccb71c1b36
Vladimir Baraniuk <norb4kivlad@gmail.com>
2025-02-10 01:49:25 +0200

    fix: fix throw ErrorButton.tsx

--- rs-react-app/src/components/ErrorBoundary.tsx
+++ rs-react-app/src/components/ErrorBoundary.tsx
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
+import { Component, ErrorInfo, PropsWithChildren, ReactNode } from 'react';
 
-interface ErrorBoundaryProps {
-  children: React.ReactNode;
+export interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
 }
 
-class ErrorBoundary extends Component<ErrorBoundaryProps> {
-  state = {
-    hasError: false,
-    error: null,
-  };
-
-  static getDerivedStateFromError(error: Error) {
-    return { hasError: true, error: error };
+class ErrorBoundary extends Component<PropsWithChildren, ErrorBoundaryState> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+    };
   }
 
-  componentDidCatch(error: Error, info: React.ErrorInfo) {
-    console.log('Error info:', error, info);
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    this.setState({ hasError: true });
+    console.log(error, errorInfo);
   }
 
-  render() {
+  render(): ReactNode {
     if (this.state.hasError) {
       return <h1>Something went wrong...</h1>;
     }
-    return this.props.children;
+    return this.props.children as ReactNode;
   }
 }
 
--- rs-react-app/src/components/ErrorButton.tsx
+++ rs-react-app/src/components/ErrorButton.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 const ErrorButton = () => {
+  const [hasError, setHasError] = useState<boolean>(false);
   const handleClick = () => {
-    throw new Error('This is a test error!');
+    setHasError(true);
   };
+
+  if (hasError) {
+    throw new Error('I crashed!');
+  }
 
   return <button onClick={handleClick}>Throw Error</button>;
 };
