7b2840122598623cb1152c4c1f24ece7a7fd51d8
Vladimir Baraniuk <norb4kivlad@gmail.com>
2025-02-09 17:39:27 +0200

    refactor: refactor CardList component from class to functional

--- rs-react-app/src/components/CardList.tsx
+++ rs-react-app/src/components/CardList.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import Card from './Card.tsx';
 import { Character } from '../interfaces/character.ts';
 
@@ -6,16 +5,14 @@ interface CardListProps {
   data: Character[];
 }
 
-class CardList extends Component<CardListProps> {
-  render() {
-    return (
-      <>
-        {this.props.data.map((character, index) => (
-          <Card info={character} key={index} />
-        ))}
-      </>
-    );
-  }
-}
+const CardList = (props: CardListProps) => {
+  return (
+    <>
+      {props.data.map((character, index) => (
+        <Card info={character} key={index} />
+      ))}
+    </>
+  );
+};
 
 export default CardList;
