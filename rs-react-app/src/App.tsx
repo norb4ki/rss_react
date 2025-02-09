b195c0bbe0e2a78e99bb5178907f824976341cd4
Vladimir Baraniuk <norb4kivlad@gmail.com>
2025-02-09 18:25:59 +0200

    refactor: refactor App component from class to functional

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,45 +1,29 @@
-import { Component } from 'react';
 import SearchPanel from './components/SearchPanel.tsx';
 import CardList from './components/CardList.tsx';
 import { Character } from './interfaces/character.ts';
 import ErrorButton from './components/ErrorButton.tsx';
 import Loader from './components/Loader.tsx';
 
-class App extends Component {
-  state = {
-    fetchResults: [],
-    isLoading: false,
-  };
+import React from 'react';
 
-  setLoading = (isLoading: boolean) => {
-    this.setState({
-      isLoading: isLoading,
-    });
-  };
+const App = () => {
+  const [fetchResults, setFetchResults] = React.useState<Character[]>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-  loadResults = (results: Character[]) => {
-    this.setState({
-      fetchResults: results,
-    });
-    console.log('Results loaded:', results);
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Star Wars Search Engine</h1>
-        {this.state.isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            <SearchPanel setLoading={this.setLoading} load={this.loadResults} />
-            <ErrorButton />
-            <CardList data={this.state.fetchResults} />
-          </>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Star Wars Search Engine</h1>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <SearchPanel setLoading={setIsLoading} load={setFetchResults} />
+          <ErrorButton />
+          <CardList data={fetchResults} />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
